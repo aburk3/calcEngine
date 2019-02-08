a99312876b0c3e7a1d77693496eb653fb281b5e8
aburk3 <aburk3@gmail.com>
2019-02-08 06:20:05 -0600

    removed unneeded comments

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -3,10 +3,6 @@ package com.pluralsight.calcengine;
 public class Main {
 
   public static void main(String[] args) {
-    // double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-    // double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-    // char[] opCodes = {'d', 'a', 's', 'm'};
-    // double[] results = new double[opCodes.length];
 
     MathEquation[] equations = new MathEquation[4];
     equations[0] = create(100.d, 50.0d, 'd');
