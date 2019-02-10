96e519e41af895e3577989b6001e4e1c8dac5696
aburk3 <aburk3@gmail.com>
2019-02-10 11:59:35 -0600

    Added StringBuilder class, primitive wrapper class, enum class

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -24,6 +24,19 @@ public class Main {
     int leftInt = 9;
     int rightInt = 4;
 
+    String[] statements = {
+            "divide 100.0 50.0",    // 100.0 / 50.0 = 2.0
+            "add 25.0 29.0",        // 25.0 + 92.0 = 117.0
+            "subtract 225.0 17.0",  // 225.0 - 17.0 = 108.0
+            "multiply 11.0 3.0"     // 11.0 * 3.0 = 33.0
+    };
+
+    CalculateHelper helper = new CalculateHelper();
+    for(String statement:statements) {
+      helper.process(statement);
+      System.out.println(helper);
+    }
+
     MathEquation equationOverload = new MathEquation('d');
 
     equationOverload.execute(leftDouble, rightDouble);
